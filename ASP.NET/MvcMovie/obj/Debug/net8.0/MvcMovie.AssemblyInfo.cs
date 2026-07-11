83d4b00090483f18b41b6f39014bdd9c470e86fb
Herzan <46695573+Herzan@users.noreply.github.com>
2026-07-10 20:21:31 -0600

    Hertz

--- ASP.NET/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ ASP.NET/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10c6b4ecc3c6b1426982bfe7a7eac2404ecbc07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a13e07567d912704c503ded8d80efe121ab3cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
