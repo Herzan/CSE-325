2f3c4c5e55e2a3a045ab58fab437b600e4c7ac34
Herzan <46695573+Herzan@users.noreply.github.com>
2026-07-30 10:41:47 -0600

    df

--- ASP.NET/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ ASP.NET/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc71eeb66f95bf6125d9afa041748cd2e970f6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04eeeba260c64ad75853a807f18251caf269478e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
